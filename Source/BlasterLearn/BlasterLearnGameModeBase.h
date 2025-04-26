ef4f729fc2bacc80a4f15320f53bf09c9d0c9ce9
坍缩的奇点 <DefineMyself2015@Gmail.com>
2025-04-26 15:21:08 +0800

    init

---
+++ Source/BlasterLearn/BlasterLearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlasterLearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERLEARN_API ABlasterLearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
