6be7c3c8d8bdf196baf5bdec362b4e2bf3d02cb9
wormh01e <DefineMyself2015@Gmail.com>
2025-06-15 14:03:46 +0800

    add scatter for hitScan weapon

--- Source/BlasterLearn/Weapon/HitScanWeapon.h
+++ Source/BlasterLearn/Weapon/HitScanWeapon.h
@@ -18,14 +18,17 @@ public:
 
 protected:
 	FVector TraceEndWithScatter(const FVector& TraceStart, const FVector& HitTarget);
-
-private:
-	UPROPERTY(EditAnywhere)
-	float Damage = 20.f;
+	void WeaponTraceHit(const FVector& TraceStart, const FVector& HitTarget, FHitResult& OutHit);
 
 	UPROPERTY(EditAnywhere)
 	class UParticleSystem* ImpactParticles;
 
+	UPROPERTY(EditAnywhere)
+	USoundCue* HitSound;
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 20.f;
+private:
 	UPROPERTY(EditAnywhere)
 	UParticleSystem* BeamParticles;
 
@@ -34,9 +37,6 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	USoundCue* FireSound;
-
-	UPROPERTY(EditAnywhere)
-	USoundCue* HitSound;
 
 	// trace end with scatter
 	UPROPERTY(EditAnywhere, Category = "Weapon Scatter")
