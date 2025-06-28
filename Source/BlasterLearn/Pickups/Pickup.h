80574423fde1dc235385844058006c5d7fc10479
wormh01e <DefineMyself2015@Gmail.com>
2025-06-28 10:31:13 +0800

    add pickup base class

---
+++ Source/BlasterLearn/Pickups/Pickup.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BLASTERLEARN_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnShpereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* PickupSound;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+};
